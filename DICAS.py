f07cbdfeabb04145bcf7202a8debf819162c4f21
clenilton-69 <faelwx032@gmail.com>
2025-08-10 10:53:50 -0300

    Adiciona imagens e arquivos do projeto

---
+++ DICAS.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.title('DICAS EMPRESARIAIS')
+
+st.write("COFIRA AQUI AS DICAS EMPRESARIAIS")
+st.write('Onde você pode encontrar dicas valiosas para o seu negócio, desde estratégias de marketing até gestão financeira.')
+
+st.image("stonks-meme.jpg", use_container_width=True)
+
+st.markdown('**E Quem Desenvolveu o Projeto Foi:**')
+
+st.write('**Rafael de Paula**')
+
+st.write('**Gustavo de Antony**')
+
+st.write('**Gabriel Silva**')
---
+++ PAGES/MARKETING.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.title('MARKETING')
+
+st.markdown("OS 4PS DO MARKETING")
+st.write('O marketing se baseia no conceito dos 4Ps para verificar as informações primárias do cliente, para então analisar soluções viáveis em cada negócio. Esse nome existe pelo fato de as informações estarem divididas em 4 pilares básicos das estratégias. Eles são referentes as seguintes palavras iniciadas com a letra “P”:')
+
+st.write('- 👜Produto: apesar do nome, deve-se incluir também os serviços ofertados. Dentro de produto deve-se incluir as características do que é vendido. É preciso definir quais as necessidades que o produto resolve, como ele é, como funciona e onde será utilizado. ')
+
+st.write('- 💲Preço: é preciso definir o valor do produto, como ele será pago, quais as condições de pagamento e se haverá descontos. O preço deve ser competitivo e atrativo para o cliente, mas também rentável para a empresa.')
+
+st.write('- 🏪Praça: é o local onde o produto será vendido, que pode ser uma loja física, um site de e-commerce ou uma plataforma de redes sociais. É importante escolher os canais de venda mais adequados para o público-alvo e garantir que o produto esteja disponível onde o cliente espera encontrá-lo.')
+
+st.write('- ⚠️Promoção: envolve as estratégias de divulgação e comunicação do produto, como publicidade, promoções, eventos e redes sociais. O objetivo é atrair a atenção do cliente, gerar interesse e estimular a compra. É importante escolher os canais de promoção mais adequados para o público-alvo e criar mensagens que ressoem com suas necessidades e desejos.')
+
+st.markdown('**E Porque o Marketing é tão importante?**')
+
+st.write('- 📱Atrai clientes e aumenta as vendas')
+st.write('- 💻Constrói a imagem da marca')
+st.write('- 📈Ajuda a entender o mercado e a concorrência')
+st.write('- 🛜Conecta com outras áreas da empresa, como vendas e atendimento.')
+
+st.image('marketing.jpg', use_container_width=True)
