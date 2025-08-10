c840b0a0271e43eb157cd02a07b857cc4bb2db68
clenilton-69 <faelwx032@gmail.com>
2025-08-10 17:09:00 -0300

    Add files via upload

---
+++ PUBLICO.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("🫂Público Alvo🫂")
+
+st.markdown("**CONHEÇA SEU PUBLICO ALVO**")
+
+st.write('Compreender as necessidades e expectativas dos clientes é um elemento fundamental para o sucesso de qualquer negócio. No cenário do varejo, que é considerado altamente competitivo hoje, as empresas que conseguem decifrar as demandas e desejos de seus clientes estão em uma posição privilegiada para oferecer produtos e serviços que realmente possam atender às suas necessidades.')
+
+st.write('A análise do público-alvo é uma ferramenta poderosa que permite às empresas identificar e compreender melhor quem são seus clientes, o que eles valorizam e como se comportam. Com essas informações, as empresas podem personalizar suas ofertas, melhorar a experiência do cliente e, consequentemente, aumentar a satisfação e a lealdade dos consumidores.')
+
+st.image('pessoas.jpg',use_container_width=True)
