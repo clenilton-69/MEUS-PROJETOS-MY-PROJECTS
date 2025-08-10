cef24fdcc640ef594cc38045f2b75194e66244a8
clenilton-69 <faelwx032@gmail.com>
2025-08-10 17:14:55 -0300

    Atualizando imagem e script principal

--- DICAS.py
+++ DICAS.py
@@ -7,7 +7,7 @@ st.title('DICAS EMPRESARIAIS')
 st.write("COFIRA AQUI AS DICAS EMPRESARIAIS")
 st.write('Onde você pode encontrar dicas valiosas para o seu negócio, desde estratégias de marketing até gestão financeira.')
 
-st.image("stonks-meme.jpg", use_container_width=True)
+st.image("stonks.jpg", use_container_width=True)
 
 st.markdown('**E Quem Desenvolveu o Projeto Foi:**')
 
