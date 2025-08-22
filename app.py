ff5c31e393205afc81a29cd3df799a0543bb96de
clenilton-69 <faelwx032@gmail.com>
2025-08-21 22:26:08 -0300

    app.py

--- app.py
+++ app.py
@@ -6,8 +6,14 @@ pagina = st.sidebar.radio("Navegue pelo app", ["Início", "Marketing", "Financei
 
 if pagina == "Início":
     st.title("💼 DICAS EMPRESARIAIS")
-    st.write("Confira aqui as dicas empresariais.")
+   st.markdown("💡 Aqui você encontra estratégias para impulsionar seu negócio.")
+st.write('Onde você pode encontrar dicas valiosas para o seu negócio, desde estratégias de marketing até gestão financeira.')
+
+col1, col2 = st.columns(2)
+
+with col1:
     st.image("stonks.jpg", use_container_width=True)
+
     st.markdown("**Quem Desenvolveu o Projeto Foi:**")
     st.write("**Rafael de Paula**")
     st.write("**Gustavo de Antony**")
@@ -27,3 +33,4 @@ elif pagina == "Equipe":
     st.title("👨‍💻 Equipe do Projeto")
     st.write("Conheça quem está por trás do projeto.")
     st.image("equipe.jpg", use_container_width=True)
+
